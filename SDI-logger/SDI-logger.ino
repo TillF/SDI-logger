ed5883691c4656f494c09e34dbf34d5c8e26bb33
TillF <francke@uni-potsdam.de>
2020-07-03 14:33:52 +0200

    merged, increased version number

--- SDI-logger/SDI-logger.ino
+++ SDI-logger/SDI-logger.ino
@@ -2,7 +2,7 @@
 //Till Francke, 2020
 
 //see instructions at https://github.com/TillF/SDI-logger
-#define ver_string "1.31"
+#define ver_string "1.32"
 #include "setup_general.h" //adjust your board settings in this file
 #include "setup_sdi.h"     //include this if you want to use SDI-12-devices
 
